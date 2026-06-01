476833145f32ac0b667dc925c21e8c6e7091d397
ne-supanat <ais-scharoenwong@ekkremis-co.ltd>
2026-06-01 06:15:34 +0000

    Deploying to gh-pages from @ ne-supanat/my-news@5e1e7e7910cf874b1debb309cc4696b74b0804f8 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3807578721" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "695905283" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
