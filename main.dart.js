3e0258b3c81c96dd7936536a405dbde61548ab45
ne-supanat <ais-scharoenwong@ekkremis-co.ltd>
2026-06-14 03:41:55 +0000

    Deploying to gh-pages from @ ne-supanat/my-news@ba511061f97fa5638bd153cc94b411ef432694ab 🚀

--- main.dart.js
+++ main.dart.js
@@ -21122,7 +21122,7 @@ $iCv:1}
 A.a8h.prototype={
 $1(a){var s=A.cf().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/4c525dac5ebe5971c5708ef73558ed8edcf4a362/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
 $S:45}
 A.Fe.prototype={
 gz7(){var s=this.b
